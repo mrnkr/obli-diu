48717eb2b0e8394734d700f1d431f7e4d753f7e5
Alvaro Nicoli <xmr.nkr@gmail.com>
2021-09-21 13:21:55 -0300

    fix: material ui bugs

--- pages/_app.js
+++ pages/_app.js
@@ -1,15 +1,38 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import Head from 'next/head';
+import CssBaseline from '@material-ui/core/CssBaseline';
 import ThemeProvider from '@material-ui/styles/ThemeProvider';
+import { ApolloProvider } from '@apollo/client';
 
 import '../styles/globals.css';
 import theme from '../theme';
+import useMountEffect from '../hooks/useMountEffect';
+import client from '../apollo/config';
 
-const MyApp = ({ Component, pageProps }) => (
-  <ThemeProvider theme={theme}>
-    <Component {...pageProps} />
-  </ThemeProvider>
-);
+const MyApp = ({ Component, pageProps }) => {
+  useMountEffect(() => {
+    // Remove the server-side injected CSS.
+    const jssStyles = document.querySelector('#jss-server-side');
+    if (jssStyles) {
+      jssStyles.parentElement.removeChild(jssStyles);
+    }
+  }, []);
+
+  return (
+    <>
+      <Head>
+        <title>Obligatorio DIU</title>
+      </Head>
+      <ApolloProvider client={client}>
+        <ThemeProvider theme={theme}>
+          <CssBaseline />
+          <Component {...pageProps} />
+        </ThemeProvider>
+      </ApolloProvider>
+    </>
+  );
+};
 
 MyApp.propTypes = {
   Component: PropTypes.any,
